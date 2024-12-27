fa4555c4c206665b38816a9f890beeb2ca720f2b
Hubert240 <szpiners@gmail.com>
2024-12-27 03:37:47 +0100

    home site

--- catalog_frontend/src/components/home/Home.jsx
+++ catalog_frontend/src/components/home/Home.jsx
@@ -11,8 +11,8 @@ function Home() {
     const fetchAudioCount = async () => {
       try {
         if (userIsAuthenticated()) {
-          const user = getUser(); // Get the user inside the useEffect hook
-          const response = await catalogApi.numberOfAudio(user,user.id);
+          const user = getUser();
+          const response = await catalogApi.numberOfAudio(user, user.id);
           setAudioCount(response.data);
         }
       } catch (error) {
@@ -21,13 +21,37 @@ function Home() {
     };
 
     fetchAudioCount();
-  }, [userIsAuthenticated, getUser]); // Add dependencies to useEffect
+  }, [userIsAuthenticated, getUser]);
+
+  const isAuthenticated = userIsAuthenticated();
 
   return (
     <div className={styles.homecontainer}>
       <h1>Witaj w aplikacji MusixList</h1>
-      {userIsAuthenticated() && <p>Aktualnie przechowujesz {audioCount} plików</p>}
-      {!userIsAuthenticated() && <p>Zarejestruj się aby katalogować swoje pliki</p>}
+      {isAuthenticated ? (
+        <>
+          <p>Aktualnie przechowujesz {audioCount} plików</p>
+          <div className={styles.description}>
+            <div className={styles.tab}>
+              <h2>Dodaj nowy plik:</h2>
+              <p>W tej zakładce możesz wgrać plik audio oraz przypisać do niego metadane takie jak tytuł, artysta, album i inne.</p>
+            </div>
+            <div className={styles.tab}>
+              <h2>Wgraj plik z metadanymi:</h2>
+              <p>Ta zakładka automatycznie wgrywa plik audio i odczytuje metadane z pliku, takie jak tytuł, artysta i album.</p>
+            </div>
+            <div className={styles.tab}>
+              <h2>Wyszukaj dane pliku:</h2>
+              <p>W tej zakładce możesz wgrać plik audio, a aplikacja automatycznie poszuka jego metadane w MusicBrainz i Artchive.</p>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <p>Zaloguj się aby dodawać nowe pliki</p>
+          <p>Jeśli nie masz konta kliknij przycisk rejestracja</p>
+        </>
+      )}
     </div>
   );
 }
